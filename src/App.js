9da7f4948bcfb600064705696abcc4c100a216e9
Susan Tan <onceuponatimeforever@gmail.com>
2019-05-10 23:14:26 -0700

    first commit, pg 57

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'; import './App.css';
 
-function App() {
-  return (
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, ];
+
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class App extends Component {
+
+  constructor(props) {
+   super(props);
+   this.state = {
+      list,
+      searchTerm: ''
+   };
+   // this.onDismiss = this.onDismiss.bind(this);
+  }
+
+
+  onDismiss(id) {
+    console.log("onDismiss starts");
+    function isNotId(item) {
+      return item.objectID !== id;
+    }
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <form>
+        <input type="text" onChange={this.onSearchChange} />
+      </form>
 
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => {
+
+          const onHandleDismiss = () => this.onDismiss(item.objectID);
+
+           return (
+            <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+             </span>
+             <span>{item.author}</span>
+             <span>{item.num_comments}</span>
+             <span>{item.points}</span>
+             <span>
+                <button
+                  onClick={onHandleDismiss}
+                  type="button"> Dismiss
+                </button>
+              </span>
+
+             </div>);
+        })}
+    </div>);
+  }
+  }
 export default App;
